e1d0b5876d5ec921117953d26b7c6acc3d44d8d0
isaacruano713 <isaacruano713@gmail.com>
2024-08-25 17:09:47 -0400

    Separation of LinearReg and other classes/utils
    1. The LinearForm class was removed from this file and are now found in the LinearForm.py file
    
    2. Uitlity functions were removed from this file and are now found in the util.py file
    
    3. The fit method was updated with the new dframe_to_intmatrix utilility method
    
    4. More documentation was added for various functions, and the documentation was made a bit neater
    
    5. The pred method was updated to allow a larger variety of inputs for the data parameter
    
    6. The resid method was created to calculate residuals
    
    7. All testing done at the bottom was wrapped in the main function and guarded by checking if __name__ is "__main__" to prevent the code from running in other scripts if the LinearReg module is imported

--- LinearReg.py
+++ LinearReg.py
@@ -1,139 +1,50 @@
-import os
-from typing import Any, Self, Union
+from typing import Union
 import numpy as np
 import pandas as pd
-
-def breakline(char: str='-') -> str:
-    return char * os.get_terminal_size().columns
-
-
-def heading(text: str, char: str='-') -> str:
-    """
-        Creates a string where {text} is centered followed by a line of characters {char}
-        that fill the width of the console
-
-        Try: print(heading("chicken"))
-    """
-    cw = os.get_terminal_size().columns
-    return f"{text.center(cw)}\n{cw*char}"
-
-def arr_merge(arr1: Union[list[Any], np.array], arr2: Union[list[Any], np.array]) -> np.array:
-    """
-        Takes two arrays and picks items from them alternately to combine into a single
-        array
-    """
-    n1: int = len(arr1)
-    n2: int = len(arr2)
-    n: int = max(n1, n2)
-    N: int = n1 + n2
-    # I would love to know a more efficient way of doing this.
-    # Using np.empty creates errors, presumably due to string length and memory allocation.
-    result = np.concatenate((arr1, arr2))
-    index: int = 0
-    for i in range(n):
-        if i < n1:
-            result[index] = arr1[i]
-            index += 1
-        if i < n2:
-            result[index] = arr2[i]
-            index += 1
-    return result
-
-
-def str_merge(str_arr1: Union[list[str], np.array],
-              str_arr2: Union[list[str], np.array],
-              delim: str="") -> np.array:
-    """
-        Takes two arrays of strings and picks items from the arrays alternately to combine
-        into a single string
-    """
-    return delim.join(arr_merge(str_arr1, str_arr2))
-
-
-class LinearForm:
-    """
-        LinearForm objects are formulas that can be used to create LinearReg and similar
-        objects.
-
-        The basic way of writing these formulas is as follows:
-            LinearForm("y = x1 + x2 + x3 + ...")
-        where y is the name of the dependent variable used in the linear regression model,
-        and x1, x2, x3, ... are all of the dependent variables for the model.
-
-        Keep in mind that the names used for y, x1, x2, ..., should all match the column
-        names in the DataFrame used to create the model.
-
-        Try:
-            LinearForm("money = time + effort")
-    """
-
-
-    def __init__(self, formula: str):
-        self.formula = formula
-        vars = formula.split("=")
-        self.y = vars[0].strip()
-        self.x = [x.strip() for x in vars[1].split('+')]
-        self.is_pretty = False
-
-    def prettify(self) -> None:
-        """
-            Reformats the formula to make it look neater.
-
-            A formula like y=x+x2 will then become y = x + x2.
-        """
-        if not self.is_pretty:
-            self.formula = f"{self.y} = {" + ".join(self.x)}"
-            self.is_pretty = True
-
-    def __repr__(self) -> None:
-        if not self.is_pretty:
-            self.prettify()
-        return f"{heading("LinearForm")}\nFormula: {self.formula}\nx: {self.x}\ny: {self.y}"
-
-    @staticmethod
-    def from_list(x: list[str], y: str) -> Self:
-        """
-            This is an alternative method of creating a LinearForm object if it is more convenient
-            to supply the names of the variables in a list that it is to write out the formula.
-            
-            Try: LinearForm.from_list(['x1', 'x2', 'x3'], 'y')
-        """
-        return LinearForm("{}={}".format(y, "+".join(x)))
+import util as u
+from LinearForm import LinearForm
 
 
 class LinearReg:
     """
-        --- Purpose ---
-        LinearReg objects are used to fit and analyze linear models.
+    --- Purpose ---
+    LinearReg objects are used to fit and analyze linear models.
 
-        
+    
 
-        --- Properties ---
-        formula(LinearForm): LinearForm object that shows the dependent ("y") and independent ("x")
-        variables of the LinearReg object
+    --- Properties ---
+    formula(LinearForm): LinearForm object that shows the dependent ("y") and independent ("x")
+    variables of the LinearReg object
 
-        y(np.array[float]): 1-dimensional numpy array containing the data for the dependent variable
+    y(np.array[float]): 1-dimensional numpy array containing the data for the dependent variable
 
-        x(np.array[float]): 2-dimensional numpy array containing the data for the independent variables
-        -   includes a column of 1's appended to the beggining of the array
+    x(np.array[float]): 2-dimensional numpy array containing the data for the independent variables
+    -   includes a column of 1's appended to the beggining of the array
 
-        yvar(str): string that holds the name of the dependent variable
+    yvar(str): string that holds the name of the dependent variable
 
-        xvars(list[str]): a list of strings containing the names of the independent variables
+    xvars(list[str]): a list of strings containing the names of the independent variables
 
-        n(int): an integer tracking the number of rows/observations in the data
-        -   equal to the length of y or the length of the first dimension of x
+    n(int): an integer tracking the number of rows/observations in the data
+    -   equal to the length of y or the length of the first dimension of x
 
-        k(int): an integer that tracks the number of independent ("x") variables
+    k(int): an integer that tracks the number of independent ("x") variables
 
-        B(np.array[float]): 1-dimensional numpy array containing the parameter estimates
-        for the linear regression model
+    B(np.array[float]): 1-dimensional numpy array containing the parameter estimates
+    for the linear regression model
 
-        model(str): a string representation of the fitted linear model
+    model(str): a string representation of the fitted linear model
 
 
 
-        --- Methods ---
+    --- Methods ---
+    Instance Methods
+        __init__(self) -> Self
+        -   initializes the LinearReg object
+
+        __repr__(self) -> str
+        -   creates a string representation of the LinearReg object when printing
+
         write_model(self) -> None
         -   creates a string representation of the fitted linear model
 
@@ -149,6 +60,10 @@ class LinearReg:
         -   uses the linear model to calculate residuals for model predictions
         -   if no arguments are passed, it returns the residuals using the data provided
             to fit the model
+
+        test(self, alt: list[LinearForm]) -> None
+        -   tests the significance of the parameters in the model
+        -   prints the information it finds
     """
     def __init__(self):
         self.formula = None
@@ -163,9 +78,10 @@ class LinearReg:
 
     def write_model(self) -> None:
         """
-            Internal function that is used to write a representation of the linear model. If model is None then
-            the function will create this representation using the parameter estimates and the variable names.
-            Otherwise 
+        --- Purpose ---
+        Internal function that is used to write a representation of the linear model.
+        If model is None then the function will create this representation using the
+        parameter estimates and the variable names.
         """
         if self.formula:
             signs = [" + " if x >= 0 else " - " for x in self.B[1:]]
@@ -176,7 +92,7 @@ class LinearReg:
             x_chars = np.char.add(x_chars, ")")
             x_chars = np.concatenate((np.array([""]), x_chars))
             xvars = np.char.add(B_chars, x_chars)
-            self.model = f"{self.yvar} = {str_merge(xvars, signs)}"
+            self.model = f"{self.yvar} = {u.str_merge(xvars, signs)}"
 
     ##############################################################
     # Here are all the methods that involve actual data analysis #
@@ -185,26 +101,26 @@ class LinearReg:
 
     def fit(self, formula: Union[str, LinearForm], dframe: pd.DataFrame) -> None:
         """
-            --- Purpose ---
-            Fits the linear model to the data
+        --- Purpose ---
+        Fits the linear model to the data
 
-            
+        
 
-            --- Parameters ---
-            formula: either a string or a LinearForm that indicates what the dependent ("y") varaiable
-            of the model is and what the independent ("x") variables of the model are.
-            -   Ex. "y = x1 + x2" or LinearForm("y = x1 + x2")
+        --- Parameters ---
+        formula: either a string or a LinearForm that indicates what the dependent ("y") varaiable
+        of the model is and what the independent ("x") variables of the model are.
+        -   Ex. "y = x1 + x2" or LinearForm("y = x1 + x2")
 
-            pdframe: a pandas DataFrame containing the data that will be used for the model
+        pdframe: a pandas DataFrame containing the data that will be used for the model
 
-            
+        
 
-            --- Return Type ---
-            None
+        --- Return Type ---
+        None
 
 
 
-            --- Examples ---
+        --- Examples ---
 
 
 
@@ -226,7 +142,7 @@ class LinearReg:
         self.k = len(self.xvars)
         self.n = dframe.shape[0]
         self.y = dframe[self.yvar].to_numpy()
-        self.x = pd.concat((pd.DataFrame(np.ones(self.n, dtype=int)), dframe[self.xvars]), axis=1).to_numpy()
+        self.x = u.dframe_to_intmatrix(dframe, self.xvars)
         self.model = None
 
         xtx = np.transpose(self.x) @ self.x
@@ -237,41 +153,113 @@ class LinearReg:
         self.C = C
         self.B = B
 
-    def pred(self, data: np.array=None) -> np.array:
+    ###############################################################################
+    def pred(self, data: Union[np.array, list[float], pd.DataFrame, None]=None) -> np.array:
         """
-            --- Purpose ---
-            Uses the linear model to return model predictions
-            If no arguments are passed, it returns the predictions using the data provided
-            to fit the model.
-            
-
-            --- Parameters ---
-            data: a 2-dimensional numpy array containing the data you want to make your predictions
-            -   make sure that the column names match the dependent variables
-            -   make sure that you have an additional column of 1's in the 0 column position and that
-                you have a column for every independent ("x") variable in addition to the column of 1's
-
-            
-
-            --- Return Type ---
-            np.array[float]
+        --- Purpose ---
+        Uses the linear model to return model predictions
+        If no arguments are passed, it returns the predictions using the data provided
+        to fit the model.
+        
 
 
+        --- Parameters ---
+        data(Union[np.array, list[float], pd.DataFrame, None]): a 2-dimensional numpy
+        array, a list of floats, or a pandas DataFrame containing the data that you want
+        to make predictions on
+        -   For all of the options supplied for {data}, do not include an intercept value,
+            or column; an intercept value or column will automatically be added
+        -   If {data} is a numpy array, it must be 2-dimensional, it must have all of its
+            columns in the same order as they are in the formula, and it must include only
+            the columns for the independent ("x") variables.
+        -   If {data} is a pandas DataFrame, make sure it has a column for each of the
+            independent variables, and make sure that the column names match up with the
+            names of the variables in {self.xvars}, but the columns do not need to be in
+            the same order as {self.xvars}
+        -   If {data} is a list, it must be a 1-dimensional list used to calculate only one
+            prediction. It must also contain values for all of the dependent variables in
+            the order that they are presented in {self.xvars}
+        -   default value: None
 
-            --- Examples ---
-            import pandas as pd
-            import numpy as np
-            data = pd.DataFrame({"x": [1, 2, 3, 4, 5, 6, 7, 8], "y": [2, 5, 4, 9, 4, 7, 15, 11]})
+        
 
-            lm = LinearReg()
-            lm.fit("y = x", data)
-            print(lm.y)
-            print(lm.pred())
-            print(lm.pred(pd.DataFrame({0: [1], "x":[9]})))
+        --- Return Type ---
+        np.array[float]
+
+
+
+        --- Examples ---
+        data = pd.DataFrame({"x": [1, 2, 3, 4, 5, 6, 7, 8], "y": [2, 5, 4, 9, 4, 7, 15, 11]})
+
+        lm = LinearReg()
+        lm.fit("y = x", data)
+        print(lm)
+        print(lm.pred())
+        print(lm.pred(pd.DataFrame({"x":[9]})))
+        print(lm.pred([9]))
+        try:
+            print(lm.pred(9))
+        except TypeError:
+            print("You cannot enter a simple data type. You must use a list, array, or DataFrame.")
+        print(lm.pred(np.array([5, 6, 7, 8]).reshape((4, 1))))
         """
-        if data:
-            return data @ self.B
-        return self.x @ self.B
+        if isinstance(data, np.ndarray):
+            return u.array_to_intmatrix(data) @ self.B
+        if isinstance(data, list):
+            data.insert(0, 1)
+            return np.array([np.dot(data, self.B)])
+        if isinstance(data, pd.DataFrame):
+            return u.dframe_to_intmatrix(data, self.xvars) @ self.B
+        if not data:
+            return self.x @ self.B
+        raise TypeError("Parameter data is of an unidentified type. Must be a list, DataFrame or numpy array.")
+
+    def resid(self,
+              data: Union[np.array, list[float], pd.DataFrame, None]=None,
+              y: Union[np.array, list[float]]=None) -> np.array:
+        """
+        --- Purpose ---
+        Calculates and returns the residuals of a linear model. Will return the residuals of
+        the data in {self.x} if no other data is specified.
+        
+
+
+        --- Parameters ---
+        data(Union[np.array, list[float], pd.DataFrame, None]): the data that you supply to
+        calculate the residuals
+        -   defaults to {self.x} if the value is set to None
+        -   see {self.pred()} to learn other properties for this parameter
+
+        y(Union[np.array[float], list[float]]): a list or array of floats for the true values
+        for the dependent variable of the linear model
+
+
+
+        --- Return Type ---
+        np.array[float]: an array of floats for the calulated residuals
+
+
+
+        --- Examples ---
+        data = pd.DataFrame({"x1": [1, 2, 3, 4, 5, 6, 7, 8],
+                            "x2": [5, 23, 13, 23, 33, 12, 34, 23],
+                            "y": [3, 2, 3, 5, 3, 6, 11, 2]})
+
+        lm = LinearReg()
+        lm.fit("y = x1 + x2", data)
+
+        print(lm)
+        print(lm.resid())
+        print(lm.resid([2, 9], [2]))
+        print(lm.resid(pd.DataFrame({"x1": [2, 3, 4], "x2": [39, 12, 48]}), y=[5, 3, 6]))
+        print(lm.resid(np.array([[2, 39], [3, 12], [4, 48]]), y=np.array([5, 3, 6])))
+        """
+        if y is None:
+            y = self.y
+        if isinstance(y, list):
+            y = np.array(y)
+        predictions = self.pred(data)
+        return y - predictions
 
     def __repr__(self) -> str:
         if not self.model:
@@ -285,7 +273,7 @@ class LinearReg:
         ydata_str = f"ydata:\n{self.y}\n"
         xdata_str = f"xdata:\n{self.x}\n"
         B_str = f"B: {self.B}"
-        return "\n".join([heading("LinearReg"),
+        return "\n".join([u.heading("LinearReg"),
                           formula_str,
                           model_str,
                           xvars_str,
@@ -295,11 +283,31 @@ class LinearReg:
                           xdata_str,
                           B_str])
     
-hi = pd.DataFrame({"time": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                   "money": [123, 283, 230, 304, 420, 293, 394, 392, 500, 512],
-                   "effort": [1, 2, 3, 2, 3, 2, 3, 1, 1, 2]})
+# hi = pd.DataFrame({"time": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+#                    "money": [123, 283, 230, 304, 420, 293, 394, 392, 500, 512],
+#                    "effort": [1, 2, 3, 2, 3, 2, 3, 1, 1, 2]})
 
-lm = LinearReg()
-lm.fit("money = time + effort", hi)
+# lm = LinearReg()
+# lm.fit("money = time + effort", hi)
 
-print(lm.pred())
+# print(lm.pred())
+
+################################################################################################
+# Testing
+################################################################################################
+def main():
+    data = pd.DataFrame({"x1": [1, 2, 3, 4, 5, 6, 7, 8],
+                         "x2": [5, 23, 13, 23, 33, 12, 34, 23],
+                         "y": [3, 2, 3, 5, 3, 6, 11, 2]})
+
+    lm = LinearReg()
+    lm.fit("y = x1 + x2", data)
+
+    print(lm)
+    print(lm.resid())
+    print(lm.resid([2, 9], [2]))
+    print(lm.resid(pd.DataFrame({"x1": [2, 3, 4], "x2": [39, 12, 48]}), y=[5, 3, 6]))
+    print(lm.resid(np.array([[2, 39], [3, 12], [4, 48]]), y=np.array([5, 3, 6])))
+
+if __name__ == "__main__":
+    main()
